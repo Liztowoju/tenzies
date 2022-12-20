a18994600e7922f9d63ee394a781f4deac0f0cde
Liz Towoju <liztowoju@gmail.com>
2022-12-20 15:42:55 +0100

    feat:roll button

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,47 @@
 import React from 'react'
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
 import Header from './components/Header'
 import Die from './components/Die'
 import './App.css'
 
-function App() {
+export default function App() {
   const [newDice, setNewDice] = React.useState(dice())
 
 
   function dice() {
     const diceMarks = []
     for (let i = 0; i < 10; i++) {
-      diceMarks.push(Math.floor(Math.random() * 6))
+      diceMarks.push(
+        {
+          value: Math.ceil(Math.random() * 6),
+          isHeld: false
+        })
+      return diceMarks
     }
-    return diceMarks
+
+
+    function rollDice() {
+      setNewDice(dice())
+    }
+
+
+    return (
+      <main className='gameboard'>
+        <Header />
+        <div className='dice-container'>
+          {newDice.map(function (die) 
+          {
+            return <Die value={die.value} />
+           })
+          }
+        </div>
+
+        <button onClick={rollDice} className='roll-button'>Roll</button>
+
+      </main>
+    )
+
+
 
   }
-  console.log(dice())
-  return (
-    <main className='gameboard'>
-      <Header />
-      <div className='dice-container'>
-      {newDice.map(function (die) {
-        return <Die value={die} />
-      })}
-      </div>
-    </main>
-  )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // function rollDice() {
-  //   setDice(allNewDice())
-  // }
-
-
-  // function selectDie() {
-  //   setSelectedDie(function () {
-
-  //   })
-  // }
-
-  // return (
-  //   <main className='gameboard'>
-  //     <Header />
-  //     <div className='dice-container'>
-
-  //       {
-  //         dice.map(function (die) {
-  //           return <Die handleClick={selectDie} value={die} />
-  //         })
-  //       }
-
-  //     </div>
-  //     <button onClick={rollDice} className='roll-button'>Roll Dice</button>
-
-  //   </main>
-  // )
-
 }
-
-export default App
